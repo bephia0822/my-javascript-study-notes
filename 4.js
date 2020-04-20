3b0eafcf5cf07961dd67ae181a0cec8d3a2230a4
bephia0822 <bephia0822@gmail.com>
2020-04-20 17:35:22 +0900

    Challenge: Template Literals

---
+++ 4.js
@@ -0,0 +1,14 @@
+// Challenge: Help Darth Vader express himself to his son!
+//const mySon = "Luke";
+//const parentalStatus = "father";
+//const message = "mySon, I am your parentalStatus";
+//console.log(message);
+
+
+
+const mySon = "Luke";
+const parentalStatus = "father";
+const message = `${mySon}, I am your ${parentalStatus}`;
+console.log(message);
+
+//看到console跑出"Luke, I am your father"的瞬間滿感動的哈哈
