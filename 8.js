86c6577d3b5bae88d5b07c3ad28c7e4a527fe1a7
bephia0822 <bephia0822@gmail.com>
2020-04-21 23:35:17 +0900

    How to shorten conditionals with ternaries

---
+++ 8.js
@@ -0,0 +1,37 @@
+const age = 20;
+
+let greeting;
+
+if (age < 10) {
+  greeting = "Hey there"; 
+} else {
+  greeting = "That's an interesting age!";  
+}
+console.log(greeting);
+
+//如何簡短表示
+
+const greeting = age < 10 ? "Hey there" : "That's an interesting age!";
+console.log(greeting);
+
+//難度提高版
+
+const age = 20;
+
+let greeting;
+
+if (age < 10) {
+  greeting = "Hey there"; 
+} else if (age > 18) {
+  greeting = "Greetings";  
+} else if (age > 10) {
+  greeting = "What's up?";  
+} else {
+  greeting = "That's an interesting age!";  
+}
+console.log(greeting);
+
+//簡化
+
+const greeting = age < 10 ? "Hey there" : age > 18 ? "Greetings" : age > 10 ? "What's up?" : "That's an interesting age!";
+console.log(greeting);
