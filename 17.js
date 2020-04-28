e7fcd7b4970aebd2d4e0df35948971ec726e6f06
bephia0822 <bephia0822@gmail.com>
2020-04-28 18:17:06 +0900

    Shorter functions with arrow functions

---
+++ 17.js
@@ -0,0 +1,15 @@
+// =>
+
+const username = 'john';
+
+const capitalize = name => `${name.charAt(0).toUpperCase()}${name.slice(1)}`;  
+ 
+function greetUser(name, callback) {
+  return callback(capitalize(name));  
+}
+
+const result = greetUser(username, name => `Hi there, ${name}!`);
+
+console.log(result);  
+
+//會跑出”Hi there, John!“
