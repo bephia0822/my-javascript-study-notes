da59e36289f1dc1417682da89a03b5d150fdde6d
bephia0822 <bephia0822@gmail.com>
2020-05-13 20:55:59 +0900

    Build flexible collections with arrays

--- 30.js
+++ 30.js
@@ -32,3 +32,22 @@ todos.push(todo1, todo2);
 // todos[0] = todo1;
 console.log(todos);
 //[{text: "Wash the dishes", complete: false}, {text: "Do laundry", complete: false}] 一樣
+
+const todos = [];
+
+const todo1 = {
+  text: 'Wash the dishes',
+  complete: false  
+};
+
+const todo2 = {
+  text: 'Do laundry',
+  complete: false  
+};
+
+todos.push(todo1, todo2);
+todos.pop();              //跟push不同的是pop只會讓你得到第一項
+// todos[1] = todo2;
+// todos[0] = todo1;
+console.log(todos);
+//[{text: "Wash the dishes", complete: false}]
