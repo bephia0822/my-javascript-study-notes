b77d2838599a6e91c514e295aab7c927748fa0e2
bephia0822 <bephia0822@gmail.com>
2020-05-20 20:20:11 +0900

    What are constructor functions

--- 45.js
+++ 45.js
@@ -1,1 +1,47 @@
-ggf
+const student1 = {
+    id: 1,
+    name: "Reed",
+    subjects: [],
+    addSubject(subject) {
+      this.subjects = [...this.subjects, subject];
+    }
+  }
+  
+  student1.addSubject('Math');
+  console.log(student1.subjects);
+  //["Math"]
+
+
+  function Student(id, name, subjects = []) {
+    this.id = id;
+    this.name = name;
+    this.subjects = subjects;  
+  }
+  
+  Student.prototype.addSubject = function(subject) {
+    this.subjects = [...this.subjects, subject];   
+  }
+  
+  const student1 = new Student(1, 'Reed');
+  
+  student1.addSubject('Math');
+  console.log(student1.subjects);
+  //["Math"]
+
+  function Student(id, name, subjects = []) {
+    this.id = id;
+    this.name = name;
+    this.subjects = subjects;  
+  }
+  
+  Student.prototype.addSubject = function(subject) {
+    this.subjects = [...this.subjects, subject];   
+  }
+  
+  const student1 = new Student(1, 'Reed');
+  const student2 = new Student(2, 'Doug');
+  
+  student1.addSubject('Math');
+  student2.addSubject('Physics');
+  console.log(student2.subjects);
+  //["Physics"]
