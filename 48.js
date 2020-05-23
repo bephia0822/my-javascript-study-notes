c3d2e4e814fd7dcc651c2d78ffa3b6d3ada97932
bephia0822 <bephia0822@gmail.com>
2020-05-23 16:54:58 +0900

    Easy prototypal inheritance with classes

--- 48.js
+++ 48.js
@@ -1,1 +1,36 @@
-ftfjkjkhjhjjjh
+// classes === constructor functions
+// classes - create objects with shared methods
+
+// function Student() {}
+
+class Student {
+    constructor(id, name, subjects = []) {
+      this.id = id;
+      this.name = name;
+      this.subjects = subjects;      
+    }  
+      
+    addSubject() {}  
+  }
+  
+  const student1 = new Student(1, 'Reed');
+  console.log(student1.id);
+  //1
+
+  class Student {
+    constructor(id, name, subjects = []) {
+      this.id = id;
+      this.name = name;
+      this.subjects = subjects;      
+    }  
+      
+    getStudentName() {
+      return `Student: ${this.name}`  
+    }
+      
+    addSubject() {}  
+  }
+  
+  const student1 = new Student(1, 'Reed');
+  console.log(student1.getStudentName());
+  //Student: Reed
